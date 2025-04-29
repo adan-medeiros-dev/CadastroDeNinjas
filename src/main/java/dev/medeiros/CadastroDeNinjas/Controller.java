c30d0a9033c394221415bc3dcdfd74c683295996
Medeiros <amedeiros.dev@gmail.com>
2025-04-29 07:14:45 -0300

    Criando primeira rota

---
+++ src/main/java/dev/medeiros/CadastroDeNinjas/Controller.java
@@ -0,0 +1,16 @@
+package dev.medeiros.CadastroDeNinjas;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping
+public class Controller {
+    @GetMapping("/helloWorld")
+    public String helloWorld(){
+        return "HelloWorld";
+    }
+
+}
