5d30424f43adc09b6934b1cced35e1eea6c17504
Adan Medeiros <amedeiros.dev@gmail.com>
2025-04-29 20:40:20 -0300

    Criada entidade Ninja

--- src/main/java/dev/medeiros/CadastroDeNinjas/NinjaController.java
+++ src/main/java/dev/medeiros/CadastroDeNinjas/NinjaController.java
@@ -1,16 +1,14 @@
 package dev.medeiros.CadastroDeNinjas;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.Mapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @RequestMapping
-public class Controller {
+public class NinjaController {
     @GetMapping("/helloWorld")
     public String helloWorld(){
         return "HelloWorld";
     }
-
 }
